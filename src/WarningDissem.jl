5ffc87c09fe0504a544c5c133b5c672dd15b80b9
Charles Koll <kollch@oregonstate.edu>
2021-07-24 17:23:03 -0700

    Finish rewrite of lattice grid network example
    Removed all globals and rewrote everything to use DataFrames; much easier to add things now.

--- src/WarningDissem.jl
+++ src/WarningDissem.jl
@@ -3,111 +3,102 @@ module WarningDissem
 # Write your package code here.
 using LightGraphs, MetaGraphs
 using Distributions
+using DataFrames
 using GLMakie, Colors
+
+const Float = AbstractFloat;
+const Range = AbstractRange;
 #using Compose, GraphPlot
 
 #dba = barabasi_albert(1000, 2; is_directed=true)
 #init_layers(n) = [barabasi_albert(n, 2), watts_strogatz(n, 4, 0.5), erdos_renyi(n, 2n)];
 
 
-#=
-g = grid((100, 100))
 # Starting with a basic ER network
 # 1001 nodes, .004 prob
 #g = erdos_renyi(100001, .00004)
 # Expected degree: 4
 # Percolation threshold p: .25
-p = .5
+#p = .5
 
-# Uniform distribution
-d = Uniform()
+get_neighbors(g, frontier, p, d) = map(x -> rand(d) ≤ p ? x : [], neighbors.(tuple(g), frontier));
 
-# How to check if a stochastic event happens
-happens(p) = rand(d) ≤ p;
+disseminate(G::Vector, n₀::Int, p::Float)::DataFrame = disseminate(G, n₀, p, Uniform());
 
-# Doing this for now because the example's a single-layer network
-init_layers(n) = [g];
-
-
-get_neighbors(g) = map(x -> rand(d) ≤ p ? x : [], neighbors.(tuple(g), frontier));
-
-n = 10000;
-G = init_layers(n);
-
-total_resultsˣ, total_resultsʸ = [], [];
-highest_resultsˣ, highest_resultsʸ = [], [];
-
-frontier = Set();
-reached = Set();
-step = 0;
-resultsʸ = [];
-
-function run(num_nodes)
-    # These are the ones we're starting from
-    nodes = [rand(1:10000) for _ ∈ 1:num_nodes];
-    global frontier = Set(nodes);
-    global reached = copy(frontier);
-    global step = 0;
-    push!(resultsʸ, length(reached));
-    #println("Starting with node: ", node);
-
-    while !isempty(frontier)
-        global step += 1;
-        # 3D vector; graph layers, frontier nodes, neighbors
-        g_neighbors = get_neighbors.(G);
-        talk_to = Iterators.flatten(Iterators.flatten(g_neighbors));
-        #println(talk_to);
-        global frontier = setdiff(Set(talk_to), reached);
-        #println(frontier);
-        union!(reached, talk_to);
-        reached_len = length(reached);
-        #println(step, ": ", reached_len);
-        push!(resultsʸ, reached_len);
-    end
-end
-=#
-
-function run(G, p, d, n₀)
+function disseminate(G::Vector, n₀::Int, p::Float, d::Distribution)::DataFrame
     # These are the nodes we're starting from
     nodes = rand(vertices(G[1]), n₀);
+
     frontier = Set(nodes);
     reached = copy(frontier);
-    step = 0;
+    t = 0;
+    dissem_total::Vector{Int} = [];
+
+    while !isempty(frontier)
+        push!(dissem_total, length(reached));
+        t += 1;
+
+        # 3D vector; graph layers, frontier nodes, neighbors
+        G_neighbors = get_neighbors.(G, tuple(frontier), p, tuple(d));
+        talked_to = Iterators.flatten(Iterators.flatten(G_neighbors));
+        frontier = setdiff(Set(talked_to), reached);
+        union!(reached, talked_to);
+    end
+
+    DataFrame(n₀ = fill(n₀, t), p = fill(p, t), t = 0:(t - 1), dissem = dissem_total)
 end
 
-#=
-function iter(num, num_starting)
-    global total_resultsˣ = [];
-    global total_resultsʸ = [];
-    #global highest_resultsˣ = [];
-    #global highest_resultsʸ = [];
-    for i ∈ 1:num
-        global resultsʸ = [];
-        #println("Iteration ", i);
-        run(num_starting)
+function monte_carlo(G::Vector, n::Int, n₀::Int, p::Float; pb = true)::DataFrame
+    d = Uniform();
+    df = DataFrame();
 
-        resultsˣ = 0:step
-        resultsʸ = convert.(Int, resultsʸ)
-        push!(total_resultsˣ, resultsˣ)
-        push!(total_resultsʸ, resultsʸ)
-        push!(highest_resultsˣ, p)
-        push!(highest_resultsʸ, last(resultsʸ))
+    progress_bar = Progress(n; enabled = pb);
+
+    for i ∈ 1:n
+        newest_run = disseminate(G, n₀, p, d);
+        newest_run.run = fill(i, nrow(newest_run));
+        append!(df, newest_run);
+
+        next!(progress_bar);
+    end
+
+    df
+end
+
+function sensitivity_analysis(G::Vector, n::Int, n₀::Union{Int,Range}, p::Union{Float,Range}; pb = true)::DataFrame
+    df = DataFrame();
+
+    progress_bar = Progress(length(n₀) * length(p); enabled = pb);
+
+    for n₀ᵢ ∈ n₀, pᵢ ∈ p
+        append!(df, monte_carlo(G, n, n₀ᵢ, pᵢ; pb = false));
+
+        next!(progress_bar);
+    end
+
+    df
+end
+
+function draw_disseminate(ax, df; kwargs...)
+    lines!(ax, df.t, df.dissem; kwargs...);
+end
+
+function draw_monte_carlo(ax, df)
+    for dfᵢ ∈ groupby(df, :run)
+        draw_disseminate(ax, dfᵢ; color = RGBA(0., 0., 0., .1));
     end
 end
 
-for j ∈ 0:.01:1
-    global p = j;
-    println("p = ", p);
-    iter(1000, 100);
+function draw_sensitivity_analysis(ax, df)
+    agg_df = combine(dfᵢ -> combine(dfⱼ -> last(dfⱼ), groupby(dfᵢ, :run)), groupby(df, :p));
+    scatter!(ax, agg_df.p, agg_df.dissem; color = RGBA(0., 0., 0., 1.), markersize = 6);
 end
 
-#lines(total_resultsˣ[1], total_resultsʸ[1]; color = RGBA(0., 0., 0., .1));
-#lines!.(total_resultsˣ[2:end], total_resultsʸ[2:end]; color = RGBA(0., 0., 0., .1));
-highest_resultsˣ = convert.(Float32, highest_resultsˣ);
-highest_resultsʸ = convert.(Int, highest_resultsʸ);
-scatter(highest_resultsˣ, highest_resultsʸ; color = RGBA(0., 0., 0., 1.), markersize = 6);
-current_figure()
-=#
+G = [grid((100, 100))];
+
+fig = Figure();
+ax = Axis(fig[1, 1], xlabel = "x label", ylabel = "y label", title = "Title");
+fig
 
 #draw(SVG("test.svg"), gplot(g));
 
